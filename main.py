b05d9f08237448e76fb6db5ec82a27201f612253
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-12 10:45:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("git clone https://github.com/acexduke/SinX.git && cd SinX && python3 -m Userbot")
+os.system("git clone https://github.com/acexduke/SinX.git && cd SinX && python3 -m userbot")
